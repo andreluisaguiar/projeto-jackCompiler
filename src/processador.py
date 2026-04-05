210039199c34a8c635badfbdb7bf7026abe5dc65
andreluisaguiar <andreluisaguiar2000@gmail.com>
2026-04-04 22:20:57 -0300

    feat: preparacao de ambiente

---
+++ src/processador.py
@@ -0,0 +1,4 @@
+"""
+Preprocessador: remove comentários do código-fonte Jack
+antes de enviar ao tokenizador.
+"""
---
+++ src/xml_writer.py
@@ -0,0 +1,10 @@
+"""
+Gera o arquivo XML de saída compatível com o validador nand2tetris.
+
+Formato esperado:
+    <tokens>
+    <keyword> class </keyword>
+    <identifier> Main </identifier>
+    ...
+    </tokens>
+"""
