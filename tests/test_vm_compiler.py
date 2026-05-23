91d6ac3f1cc75f1fb2d8eb9ff5776f7c9dd617f4
andreluisaguiar <andreluisaguiar2000@gmail.com>
2026-05-22 22:18:41 -0300

    feat(codegen): gera VM para let return if while simples

--- tests/test_vm_compiler.py
+++ tests/test_vm_compiler.py
@@ -52,6 +52,91 @@ class TestJackVMCompiler(unittest.TestCase):
             ),
         )
 
+    def test_compila_let_com_argumentos_e_variavel_local(self):
+        codigo = """
+        class Main {
+           function int sum(int a, int b) {
+              var int c;
+              let c = a + b;
+              return c;
+           }
+        }
+        """
+
+        self.assertEqual(
+            compilar_codigo_jack(codigo),
+            "\n".join(
+                [
+                    "function Main.sum 1",
+                    "push argument 0",
+                    "push argument 1",
+                    "add",
+                    "pop local 0",
+                    "push local 0",
+                    "return",
+                ]
+            ),
+        )
+
+    def test_compila_while_e_if_else_com_labels_previsiveis(self):
+        codigo = """
+        class Main {
+           function void main() {
+              var int i;
+              let i = 0;
+              while (i < 2) {
+                 if (i = 1) {
+                    do Output.printInt(i);
+                 } else {
+                    do Output.printInt(0);
+                 }
+                 let i = i + 1;
+              }
+              return;
+           }
+        }
+        """
+
+        self.assertEqual(
+            compilar_codigo_jack(codigo),
+            "\n".join(
+                [
+                    "function Main.main 1",
+                    "push constant 0",
+                    "pop local 0",
+                    "label WHILE_EXP0",
+                    "push local 0",
+                    "push constant 2",
+                    "lt",
+                    "not",
+                    "if-goto WHILE_END0",
+                    "push local 0",
+                    "push constant 1",
+                    "eq",
+                    "if-goto IF_TRUE0",
+                    "goto IF_FALSE0",
+                    "label IF_TRUE0",
+                    "push local 0",
+                    "call Output.printInt 1",
+                    "pop temp 0",
+                    "goto IF_END0",
+                    "label IF_FALSE0",
+                    "push constant 0",
+                    "call Output.printInt 1",
+                    "pop temp 0",
+                    "label IF_END0",
+                    "push local 0",
+                    "push constant 1",
+                    "add",
+                    "pop local 0",
+                    "goto WHILE_EXP0",
+                    "label WHILE_END0",
+                    "push constant 0",
+                    "return",
+                ]
+            ),
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
